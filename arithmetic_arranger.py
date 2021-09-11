b9c2f83a440c9e1d0de94757e5c8e52e92cfa927
dgpaniagua <dgpaniagua1989@gmail.com>
2021-09-11 05:51:46 +0000

    lines 1,2,3 ok

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -4,12 +4,26 @@ def arithmetic_arranger(problems, solve=False):
     terms2 = list()
     signs = list()
     results = list()
+    max_digits = list()
+    top_max = list()
+    
 
+    #parses the problems
     for problem in problems:
       terms1.append(problem.split()[0])
       signs.append(problem.split()[1])
       terms2.append(problem.split()[2])
+    
+    #search for the term with the maximum number of digits
+    for i in range(len(problems)):
+      if len(terms1[i]) > len(terms2[i]):
+        max_digits.append(len(terms1[i]))
+        top_max.append(True)
+      else:
+        max_digits.append(len(terms2[i]))
+        top_max.append(False)
 
+    #solves problems if solve=True
     if solve:
       for i in range(len(problems)):
         if signs[i] == "+":
@@ -17,7 +31,26 @@ def arithmetic_arranger(problems, solve=False):
         elif signs[i] == "-":
           results.append(int(terms1[i]) - int(terms2[i]))
 
-    arranged_problems = [terms1, signs, terms2, results]
+    #line1 and line2 right justify
+    line1 = "  " + terms1[0].rjust(max_digits[0])
+    line2 = signs[0] + " " + terms2[0].rjust(max_digits[0])
+
+    for i in range(1, len(problems)):
+      line1 = line1 + "    " + "  " + terms1[i].rjust(max_digits[i])
+      line2 = line2 + "    " + signs[i] + " " + terms2[i].rjust(max_digits[i])
+
+    #dashes
+    dashes = [None] * len(problems)
+    for j in range(len(problems)):
+      dashes[j] = "--"
+      for i in range(max_digits[j]):
+        dashes[j] = dashes[j] + "-"
+        
+    line3 = dashes[0]
+    for i in range(1, len(problems)):
+      line3 = line3 + "    " + dashes[i]
+
+    arranged_problems = line1 + "\n" +line2 + "\n" + line3
 
     return arranged_problems
 
