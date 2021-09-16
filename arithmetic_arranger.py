bf6e7045c02ebb8cc5665190ceab1c44eb427611
dgpaniagua <dgpaniagua1989@gmail.com>
2021-09-16 18:15:12 +0000

    code improvements

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,11 +1,15 @@
 def arithmetic_arranger(problems, solve=False):
 
+    #variables declarations
     terms1 = list()
     terms2 = list()
     signs = list()
     results = list()
     max_digits = list()
-    max_term = list()
+    line1 = ""
+    line2 = ""
+    line3 = ""
+    line4 = ""
 
     #too many problems error:
     problems_len = len(problems)
@@ -26,57 +30,34 @@ def arithmetic_arranger(problems, solve=False):
     #search for the term with the maximum number of digits
     for i in range(problems_len):
       max_digits.append(max([len(terms1[i]), len(terms2[i])]))
-      max_term.append(max([len(terms1[i]), len(terms2[i])]))
-      #error conditions:
+      #maximum digits error:
       if max_digits[i] > 4:
         return "Error: Numbers cannot be more than four digits."
+      #only digits error
       if (not terms1[i].isdigit()) or (not terms2[i].isdigit()):
         return "Error: Numbers must only contain digits."
 
-    #line1 and line2 right justify
-    #line1 = "  " + terms1[0].rjust(max_digits[0])
-    #line2 = signs[0] + " " + terms2[0].rjust(max_digits[0])
-    line1 = ""
-    line2 = ""
-    for i in range(problems_len):
-      if i==0:
-        line1 = "  " + terms1[i].rjust(max_digits[i])
-        line2 = signs[i] + " " + terms2[i].rjust(max_digits[i])
-      else:
-        line1 = line1 + "    " + "  " + terms1[i].rjust(max_digits[i])
-        line2 = line2 + "    " + signs[i] + " " + terms2[i].rjust(max_digits[i])
-
-    #solves problems if solve=True
-    if solve:
-      for i in range(problems_len):
-        if signs[i] == "+":
-          results.append(int(terms1[i]) + int(terms2[i]))
-        else:
-          results.append(int(terms1[i]) - int(terms2[i]))
-        if results[i] > len(str(max_digits[i])):
-          max_digits[i] = len(str(results[i]))
-
-    #dashes
+    #line1, line2 and line3
     dashes = [None] * problems_len
-    for j in range(problems_len):
-      dashes[j] = "--"
-      for i in range(max_term[j]):
-        dashes[j] = dashes[j] + "-"
+    for i in range(problems_len):
+      whitespace = "" if i==0 else "    "
+      line1 = line1 + whitespace + "  " + terms1[i].rjust(max_digits[i])
+      line2 = line2 + whitespace + signs[i] + " " + terms2[i].rjust(max_digits[i])
+      dashes[i] = "--"
+      for j in range(max_digits[i]):
+        dashes[i] = dashes[i] + "-"
+      line3 = line3 + whitespace + dashes[i]
 
-    line3 = dashes[0]
-    for i in range(1, problems_len):
-      line3 = line3 + "    " + dashes[i]
+    #solves problems if solve=True and create line 4
+      if solve:
+        results.append((int(terms1[i]) + int(terms2[i])) if signs[i] == "+" else int(terms1[i]) - int(terms2[i]))
+        if i==0:
+          whitespace = "  " if results[i] >= 0 else  " "
+          justify = 0
+        else:
+          justify = 2
+        line4 = line4 + whitespace + str(results[i]).rjust(max_digits[i]+ justify)
 
-    #results
-    if solve:
-      if results[0] >= 0:
-        line4 = "  " + str(results[0]).rjust(max_digits[0])
-      else:
-        line4 = " " + str(results[0]).rjust(max_digits[0])
-      for i in range(1, problems_len):
-        line4 = line4 + "    " + str(results[i]).rjust(max_term[i]+2)
-      arranged_problems = line1 + "\n" + line2 + "\n" + line3 + "\n" + line4
-    else:
-      arranged_problems = line1 + "\n" + line2  + "\n" + line3
+      arranged_problems = line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 if solve else line1 + "\n" + line2  + "\n" + line3
 
     return arranged_problems
